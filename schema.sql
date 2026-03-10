89915f759d52b5bb6edd0e7d87ae9999a97ce18d
derek <derekhaefner@gmail.com>
2026-03-10 12:32:34 -0400

    trades table

--- schema.sql
+++ schema.sql
@@ -19,3 +19,5 @@ END;
 
 insert into transactions(name, amount, date, account, source, id, category_id) values('test', 323.98, '2026-02-04', 'citi', 'citi', '3b9a4061-245f-4f2c-8ba4-9c085fd7757e', 32);
 insert into categories(label, priority) values('test', 1);
+
+SELECT ticker, SUM(CASE WHEN type = 'sell' THEN -shares ELSE shares END) as shares FROM trades GROUP BY ticker;
